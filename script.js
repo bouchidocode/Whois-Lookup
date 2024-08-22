9d7e3a179641c5dfb60af9c8eae6439fe85b5e4f
Yօʊֆֆɛʄ <ysf.elansary@gmail.com>
2024-08-22 23:28:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,24 +9,3 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
-
-// On `result.html`, fetch data based on the domain input
-function fetchDomainData(domain) {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // Caution: for development only
-    const apiUrl = `${proxyUrl}https://api.domaintools.com/v1/domaintools.com/whois/${domain}`;
-
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            document.getElementById('apiResponse').textContent = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            document.getElementById('apiResponse').textContent = 'Error fetching data: ' + error;
-        });
-}
